dae2469da04ce1ecf2647f75fcd0a101fd47fa30
Kamil Nowak <kamil.nowak.rugby@gmail.com>
2022-01-09 20:14:37 +0100

    add split endpoint to controller server

--- controller_server/app/main.py
+++ controller_server/app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI, status
 
-from app.models import Trade
+from app.models import Trade, Split
 
 app = FastAPI()
 
 
 @app.post("/trade", status_code=status.HTTP_200_OK)
 async def fill_trade(data: Trade) -> None:
+    pass
+
+
+@app.post("/split", status_code=status.HTTP_200_OK)
+async def split_to_accounts(data: Split) -> None:
     pass
--- controller_server/app/models.py
+++ controller_server/app/models.py
@@ -1,7 +1,9 @@
 from decimal import Decimal
 from enum import Enum
+from typing import List
 
-from pydantic import BaseModel
+from fastapi import status, HTTPException
+from pydantic import BaseModel, validator
 
 
 class StockTickerEnum(Enum):
@@ -21,3 +23,17 @@ class Trade(BaseModel):
     stock_ticker: StockTickerEnum
     price: Decimal
     quantity: float
+
+
+class SplitAccount(BaseModel):
+    name: str
+    percent: float
+
+
+class Split(BaseModel):
+    accounts: List[SplitAccount]
+
+    @validator("accounts")
+    def check_percentage_split(cls, v: List[SplitAccount]) -> None:
+        if sum(account.percent for account in v) != 100:
+            raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="Sum of percentages of all accounts should be equal to 100")
