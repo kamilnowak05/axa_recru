4907b3d1864b3d4404fc68ab8c7a4b880fa8fb55
Kamil Nowak <kamil.nowak.rugby@gmail.com>
2022-01-09 19:56:21 +0100

    add trade endpoint to controller server

--- controller_server/app/main.py
+++ controller_server/app/main.py
@@ -1,4 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
+from app.models import Trade
 
 app = FastAPI()
+
+
+@app.post("/trade", status_code=status.HTTP_200_OK)
+async def fill_trade(data: Trade) -> None:
+    pass
---
+++ controller_server/app/models.py
@@ -0,0 +1,23 @@
+from decimal import Decimal
+from enum import Enum
+
+from pydantic import BaseModel
+
+
+class StockTickerEnum(Enum):
+    agilent_technologies = "A"
+    apple = "AAPL"
+    berkshire_hathaway = "BRK"
+    axa = "CS"
+    alphabet = "GOOG"
+    harley_davidson = "HOG"
+    hewlett_packard = "HPQ"
+    intel = "INTC"
+    microsoft = "MSFT"
+    petco = "WOOF"
+
+
+class Trade(BaseModel):
+    stock_ticker: StockTickerEnum
+    price: Decimal
+    quantity: float
